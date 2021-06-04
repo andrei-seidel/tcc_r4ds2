b304e238623163e73d425b2dce0395dc21efb659
clente <clente@curso-r.com>
2021-06-03 23:19:31 -0300

    Lendo os dados

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,13 @@
+library(magrittr)
+
+# Lendo os dados
+
+intermed<-purrr::map_df(.f = read.csv2,
+                        .x = c("data-raw/2015.csv",
+                               "data-raw/2016.csv",
+                               "data-raw/2017.csv",
+                               "data-raw/2018.csv",
+                               "data-raw/2019.csv"))
 
 
 
